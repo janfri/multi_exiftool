1f782517481329b67feb8e8b395327c1042c1527
Jan Friedrich <janfri@hermon.(none)>
2009-07-13 21:33:50 +0200

    Using of variant spellings of tag names in Data#[] implemented.

--- lib/multi_exiftool/data.rb
+++ lib/multi_exiftool/data.rb
@@ -3,11 +3,16 @@ module MultiExiftool
   class Data
 
     def initialize values
-      @values = values
+      @values = {}
+      values.map {|tag,val| @values[Data.unify_tag(tag)] = val}
     end
 
     def [](tag)
-      @values[tag]
+      @values[Data.unify_tag(tag)]
+    end
+
+    def self.unify_tag tag
+      tag.gsub(/[-_]/, '').downcase
     end
 
   end
--- test/test_data.rb
+++ test/test_data.rb
@@ -3,10 +3,19 @@ require 'test/unit'
 
 class TestData < Test::Unit::TestCase
 
+  def setup
+    hash = {'FNumber' => 8, 'Author' => 'janfri'}
+    @data = MultiExiftool::Data.new(hash)
+  end
+
   def test_simple
-    hash = {'FNumber' => 8}
-    data = MultiExiftool::Data.new(hash)
-    assert_equal 8, data['FNumber']
+    assert_equal 8, @data['FNumber']
+  end
+
+  def test_variant_spellings
+    assert_equal 8, @data['fnumber']
+    assert_equal 8, @data['f_number']
+    assert_equal 8, @data['f-number']
   end
 
 end
