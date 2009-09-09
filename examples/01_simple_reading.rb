430f837a60a29d5972c9b1b46658289d25608a38
Jan Friedrich <janfri.rubyforge@gmail.com>
2009-09-09 21:17:43 +0200

    Adding first example.

---
+++ examples/01_simple_reading.rb
@@ -0,0 +1,8 @@
+require 'multi_exiftool'
+
+reader = MultiExiftool::Reader.new
+reader.filenames = Dir[File.dirname(__FILE__) + '/*.png']
+data_objects = reader.read
+data_objects.each do |o|
+ puts "#{o.file_name}: #{o.comment}"
+end
