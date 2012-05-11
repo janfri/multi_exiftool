9002ecfb2a7084309547ae9b92c03c0aebdc8125
janfri <janfri26@gmail.com>
2012-05-11 21:10:07 +0200

    New example: Writing with explicit group.

---
+++ examples/04_writing_using_groups.rb
@@ -0,0 +1,33 @@
+require 'multi_exiftool'
+require 'yaml'
+
+if ARGV.empty?
+  $stderr.puts 'No filenames given.'
+  exit -1
+end
+
+puts 'Please enter a description for the given files:'
+description = $stdin.gets.chomp
+
+puts 'Please enter a caption for the given files:'
+caption = $stdin.gets.chomp
+
+writer = MultiExiftool::Writer.new
+writer.filenames = ARGV
+
+# specifying group by prefix
+writer.values = {'exif:imagedescription' => description, 'xmp:description' => description}
+
+# specifying group hierarchical
+writer.values.merge! YAML.load <<-END
+iptc:
+  caption-abstract: #{caption}
+xmp:
+  caption: #{caption}
+END
+
+if writer.write
+  puts 'ok'
+else
+  puts writer.errors.join
+end
