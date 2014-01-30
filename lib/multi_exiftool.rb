eab36391abcbc798ff542bdbe927ed03ae3566ef
Jan Friedrich <janfri26@gmail.com>
2014-01-30 18:49:49 +0100

    Update documentation.

--- lib/multi_exiftool.rb
+++ lib/multi_exiftool.rb
@@ -5,6 +5,17 @@ require_relative 'multi_exiftool/writer'
 
 module MultiExiftool
 
+  # Reading metadata
+  # Be aware: it returns an array of two elements:
+  # values, errors
+  #
+  # Example:
+  #   values, errors = MultiExiftool.read(Dir['*.jpg'])
+  #   if errors.empty?
+  #     values.each {|val| do_something_with(val) }
+  #   else
+  #     # error handling
+  #   end
   def self.read(filenames, opts={})
     reader = Reader.new
     reader.filenames = filenames
@@ -16,6 +27,14 @@ module MultiExiftool
     [values, reader.errors]
   end
 
+  # Writing metadata
+  # Returns an array of the error messages
+  #
+  # Example:
+  #   errors = MultiExiftool.write(Dir['*.jpg'], {author: 'Jan Friedrich'})
+  #   unless errors.empty?
+  #     # do error handling
+  #   end
   def self.write(filenames, values, opts={})
     writer = Writer.new
     writer.filenames = filenames
