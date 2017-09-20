56a465aa06479a39fec44609194f6f1a2486ff73
Jan Friedrich <janfri26@gmail.com>
2017-09-20 08:04:00 +0200

    Use Regtest.sample to make regtest sample ready for regtest 2.0.

--- regtest/read_all_tags.rb
+++ regtest/read_all_tags.rb
@@ -1,8 +1,6 @@
 require 'regtest'
 require 'multi_exiftool'
 
-include Regtest
-
-sample 'read all tags' do
+Regtest.sample 'read all tags' do
   MultiExiftool.read('regtest/test.jpg', tags: %w(-filemodifydate -fileaccessdate -fileinodechangedate -filepermissions))
 end
