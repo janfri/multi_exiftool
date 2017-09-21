98a158a366c1673a47597fee2608aa207590990e
Jan Friedrich <janfri26@gmail.com>
2017-09-21 12:16:27 +0200

    Add pragma frozen_string_literal.

--- lib/multi_exiftool.rb
+++ lib/multi_exiftool.rb
@@ -1,4 +1,6 @@
-# coding: utf-8
+# encoding: utf-8
+# frozen_string_literal: true
+
 require_relative 'multi_exiftool/values'
 require_relative 'multi_exiftool/reader'
 require_relative 'multi_exiftool/writer'
--- test/test_writer.rb
+++ test/test_writer.rb
@@ -1,4 +1,6 @@
-# coding: utf-8
+# encoding: utf-8
+# frozen_string_literal: true
+
 require_relative 'helper'
 
 class TestWriter < Test::Unit::TestCase
