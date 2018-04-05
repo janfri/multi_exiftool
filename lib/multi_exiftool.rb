99150d19d597588a45a9ed7d0f428f06b9a94b79
Jan Friedrich <janfri26@gmail.com>
2018-04-05 17:45:00 +0200

    Refactoring: Restructure class methods.

--- lib/multi_exiftool.rb
+++ lib/multi_exiftool.rb
@@ -9,53 +9,51 @@ module MultiExiftool
 
   VERSION = '0.8.0'
 
-  # Reading metadata
-  # Be aware: it returns an array of two elements:
-  # values, errors
-  #
-  # Example:
-  #   values, errors = MultiExiftool.read(Dir['*.jpg'])
-  #   if errors.empty?
-  #     values.each {|val| do_something_with(val) }
-  #   else
-  #     # error handling
-  #   end
-  def self.read(filenames, opts={})
-    reader = Reader.new
-    reader.filenames = filenames
-    if val = opts.delete(:tags)
-      reader.tags = val
-    end
-    if val = opts.delete(:group)
-      reader.group = val
-    end
-    reader.options = opts unless opts.empty?
-    values = reader.read
-    [values, reader.errors]
-  end
-
-  # Writing metadata
-  # Returns an array of the error messages
-  #
-  # Example:
-  #   errors = MultiExiftool.write(Dir['*.jpg'], {author: 'Jan Friedrich'})
-  #   unless errors.empty?
-  #     # do error handling
-  #   end
-  def self.write(filenames, values, opts={})
-    writer = Writer.new
-    writer.filenames = filenames
-    writer.values = values
-    writer.options = opts unless opts.empty?
-    writer.write
-    writer.errors
-  end
-
-  class Error < ::StandardError; end
-
   @exiftool_command = 'exiftool'
 
   class << self
+
+    # Reading metadata
+    # Be aware: it returns an array of two elements:
+    # values, errors
+    #
+    # Example:
+    #   values, errors = MultiExiftool.read(Dir['*.jpg'])
+    #   if errors.empty?
+    #     values.each {|val| do_something_with(val) }
+    #   else
+    #     # error handling
+    #   end
+    def read(filenames, opts={})
+      reader = Reader.new
+      reader.filenames = filenames
+      if val = opts.delete(:tags)
+        reader.tags = val
+      end
+      if val = opts.delete(:group)
+        reader.group = val
+      end
+      reader.options = opts unless opts.empty?
+      values = reader.read
+      [values, reader.errors]
+    end
+
+    # Writing metadata
+    # Returns an array of the error messages
+    #
+    # Example:
+    #   errors = MultiExiftool.write(Dir['*.jpg'], {author: 'Jan Friedrich'})
+    #   unless errors.empty?
+    #     # do error handling
+    #   end
+    def write(filenames, values, opts={})
+      writer = Writer.new
+      writer.filenames = filenames
+      writer.values = values
+      writer.options = opts unless opts.empty?
+      writer.write
+      writer.errors
+    end
 
     attr_accessor :exiftool_command
     attr_reader :exiftool_version
@@ -72,6 +70,8 @@ module MultiExiftool
     end
 
   end
+
+  class Error < ::StandardError; end
 
 end
 
