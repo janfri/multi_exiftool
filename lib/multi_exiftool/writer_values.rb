1e9e934ec863c2221246e1b2c5691f26f992152b
Jan Friedrich <janfri26@gmail.com>
2024-04-16 21:59:54 +0200

    Implement new class WriterValues

---
+++ lib/multi_exiftool/writer_values.rb
@@ -0,0 +1,89 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+module MultiExiftool
+
+  # Representing (tag, value) pairs of metadata to use with Writer#write.
+  # Access via bracket-methods or dynamic method-interpreting via
+  # method_missing.
+  class WriterValues
+
+    def initialize values={}
+      @values = {}
+      values.each_pair do |k, v|
+        self[k] = v
+      end
+    end
+
+    # Gets the value for a tag (tag will be unified, i.e. FNumber, fnumber or
+    # f_number can be used for FNumber)
+    def [] tag
+      unified_tag = MultiExiftool.unify(tag)
+      @values[unified_tag]
+    end
+
+    # Sets the value for a tag (tag will be unified, i.e. FNumber, fnumber or
+    # f_number can be used for FNumber)
+    def []= tag, val
+      unified_tag = MultiExiftool.unify(tag)
+      if val.respond_to?(:to_hash) && !val.kind_of?(WriterValues)
+        val = WriterValues.new(val)
+      end
+      @values[unified_tag] = val
+    end
+
+    def empty?
+      @values.empty?
+    end
+
+    # Gets a hash representation of this instance
+    def to_h
+      @values
+    end
+
+    alias to_hash to_h
+
+    def values_args
+      raise MultiExiftool::Error.new('No values.') if @values.empty?
+      values_to_param_array(@values).map {|arg| "-#{arg}"}
+    end
+
+    def values_to_param_array hash
+      res = []
+      hash.each do |tag, val|
+        if val.respond_to? :to_hash
+          res << values_to_param_array(val.to_hash).map {|arg| "#{tag}:#{arg}"}
+        elsif val.respond_to? :to_ary
+          res << val.map {|v| "#{tag}=#{v}"}
+        else
+          res << "#{tag}=#{val}"
+        end
+      end
+      res.flatten
+    end
+
+    private
+
+    def method_missing tag, val=nil, &block
+      unified_tag = MultiExiftool.unify(tag)
+      if unified_tag =~ /^(.+)=$/
+        self[$1] = val
+      else
+        res = self[unified_tag]
+        if block_given?
+          res = WriterValues.new unless res
+          self[unified_tag] = res
+          if block.arity > 0
+            yield res
+          else
+            res.instance_eval &block
+          end
+        end
+        res
+      end
+    end
+
+  end
+
+end
+
---
+++ test/test_writer_values.rb
@@ -0,0 +1,152 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+require_relative 'helper'
+require_relative '../lib/multi_exiftool/writer_values'
+require 'date'
+
+class TestWriterValues < Test::Unit::TestCase
+
+  context 'empty init' do
+
+    setup do
+      @values = MultiExiftool::WriterValues.new
+    end
+
+    test 'default value' do
+      assert_nil @values.iso
+      assert_nil @values.fnumber
+    end
+
+    test 'setters' do
+      @values.iso = 400
+      @values.fnumber = 5.6
+      assert_equal 400, @values['ISO']
+      assert_equal 400, @values.iso
+      assert_equal 5.6, @values.fnumber
+      assert_equal 5.6, @values.f_number
+    end
+
+    test 'values_args' do
+      assert_raises MultiExiftool::Error do
+        @values.values_args
+      end
+      @values.iso = 400
+      @values.fnumber = 5.6
+      assert_equal %w(-iso=400 -fnumber=5.6), @values.values_args
+    end
+
+  end
+
+  context 'value access' do
+
+    setup do
+      hash = {'FNumber' => 8, 'Author' => 'janfri'}
+      @values = MultiExiftool::WriterValues.new(hash)
+    end
+
+    test 'original spelling of tag name' do
+      assert_equal 8, @values['FNumber']
+      @values['FNumber'] = 5.6
+      assert_equal 5.6, @values['FNumber']
+    end
+
+    test 'variant spellings of tag names' do
+      assert_equal 8, @values['fnumber']
+      @values['fnumber'] = 5.6
+      assert_equal 5.6, @values['fnumber']
+      assert_equal 5.6, @values['f_number']
+      @values['f_number'] = 8
+      assert_equal 8, @values['f_number']
+      assert_equal 8, @values['f-number']
+      @values['f-number'] = 5.6
+      assert_equal 5.6, @values['f-number']
+    end
+
+    test 'tag access via methods' do
+      assert_equal 8, @values.fnumber
+      @values.fnumber = 5.6
+      assert_equal 5.6, @values.fnumber
+      assert_equal 5.6, @values.f_number
+      @values.f_number = 8
+      assert_equal 8, @values.f_number
+    end
+
+    test 'values_args' do
+      assert_equal %w(-fnumber=8 -author=janfri), @values.values_args
+      @values.iso = 200
+      assert_equal %w(-fnumber=8 -author=janfri -iso=200), @values.values_args
+      @values.author = nil
+      assert_equal %w(-fnumber=8 -author= -iso=200), @values.values_args
+    end
+
+  end
+
+  context 'hierarchical access' do
+
+    context 'empty init' do
+
+      setup do
+        @values = MultiExiftool::WriterValues.new
+      end
+
+      test 'block without arg' do
+        c = 'this is a comment'
+        @values.exif do
+          self.comment = c
+        end
+        assert_equal c, @values.exif.comment
+        assert_equal c, @values['EXIF'].comment
+        assert_equal c, @values.exif[:Comment]
+        assert_equal c, @values[:exif]['comment']
+        assert_equal ['-exif:comment=this is a comment'], @values.values_args
+      end
+
+      test 'block arg' do
+        c = 'this is a comment'
+        @values.exif do |exif|
+          exif.comment = c
+        end
+        assert_equal c, @values.exif.comment
+        assert_equal c, @values['exif'].comment
+        assert_equal c, @values.exif[:comment]
+        assert_equal c, @values[:EXIF]['Comment']
+        assert_equal ['-exif:comment=this is a comment'], @values.values_args
+      end
+
+    end
+
+    context 'structure init' do
+
+      setup do
+        hash = {'EXIF' => {'FNumber' => 8, 'Author' => 'janfri'}}
+        @values = MultiExiftool::WriterValues.new(hash)
+      end
+
+      test 'block without arg' do
+        assert_equal 8, @values.exif.fnumber
+        assert_equal 'janfri', @values[:exif]['Author']
+        c = 'this is a comment'
+        @values.exif do
+          self.comment = c
+        end
+        assert_equal c, @values.exif.comment
+        assert_equal ['-exif:fnumber=8', '-exif:author=janfri', '-exif:comment=this is a comment'], @values.values_args
+      end
+
+      test 'block arg' do
+        assert_equal 8, @values.exif.fnumber
+        assert_equal 'janfri', @values[:exif]['Author']
+        c = 'this is a comment'
+        @values.exif do |exif|
+          exif.comment = c
+        end
+        assert_equal c, @values.exif.comment
+        assert_equal ['-exif:fnumber=8', '-exif:author=janfri', '-exif:comment=this is a comment'], @values.values_args
+      end
+
+    end
+
+  end
+
+end
