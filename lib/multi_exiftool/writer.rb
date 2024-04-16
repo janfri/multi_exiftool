7d4b23f9b07baca613e26e8f7848d146031ba9ca
Jan Friedrich <janfri26@gmail.com>
2024-04-16 22:04:32 +0200

    Use WriterValues in Writer

--- lib/multi_exiftool/writer.rb
+++ lib/multi_exiftool/writer.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require_relative 'executable'
+require_relative 'writer_values'
 
 module MultiExiftool
 
@@ -32,32 +33,21 @@ module MultiExiftool
       cmd = []
       cmd << Writer.mandatory_args
       cmd << options.options_args
-      cmd << values_args
+      cmd << values.values_args
       cmd << filenames
       cmd.flatten
     end
 
     alias write execute # :nodoc:
 
-    private
+    alias vals values
+    alias vals= values=
 
-    def values_args
-      raise MultiExiftool::Error.new('No values.') if values.empty?
-      values_to_param_array(@values).map {|arg| "-#{arg}"}
-    end
-
-    def values_to_param_array hash
-      res = []
-      hash.each do |tag, val|
-        if val.respond_to? :to_hash
-          res << values_to_param_array(val.to_hash).map {|arg| "#{tag}:#{arg}"}
-        elsif val.respond_to? :to_ary
-          res << val.map {|v| "#{tag}=#{v}"}
-        else
-          res << "#{tag}=#{val}"
-        end
+    def values= vals
+      unless vals.kind_of? WriterValues
+        vals = WriterValues.new(vals)
       end
-      res.flatten
+      @values = vals
     end
 
   end
